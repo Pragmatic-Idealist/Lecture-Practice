a5a02147b49548efb00e2cbd6abd5a8147f60593
Michael Li <48256226+Pragmatic-Idealist@users.noreply.github.com>
2019-03-14 23:10:25 -0400

    Add files via upload

---
+++ Lecture 1 Practice Part 2 -- Something stopped working in Part 1.py
@@ -0,0 +1,250 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+numbers = [1,2,3,4,5,6,7,8,9,10]
+evenNumber = []
+count = 0
+for n in numbers:
+    if (n % 2 == 0):
+        evneNumbers.append(n)
+        count += 1
+print (evenNumbers)
+print ("There are", count, "even numbers")
+
+
+# In[3]:
+
+
+print numbers
+
+
+# In[4]:
+
+
+print (numbers)
+
+
+# In[5]:
+
+
+evenNumber = {}
+
+
+# In[6]:
+
+
+evenNumber = []
+
+
+# In[7]:
+
+
+evenNumber.append(numbers)
+
+
+# In[8]:
+
+
+print evenNumbers
+
+
+# In[10]:
+
+
+print (evenNumber)
+
+
+# In[11]:
+
+
+evenNumber = []
+
+
+# In[16]:
+
+
+numbers = [1,2,3,4,5,6,7,8,9,10]
+evenNumbers = []
+count = 0
+for n in numbers:
+    if (n % 2 == 0):
+        evenNumbers.append(n)
+        count += 1
+print (evenNumbers)
+print ("There are", count, "even numbers")
+
+
+# In[20]:
+
+
+nums =  [1,2,3,4,5,6,7,8,9,10]
+evenNums = [n for n in nums if (n%2==0)]
+print (evenNums)
+print ("There are", count, "even numbers")
+
+
+# In[24]:
+
+
+nums = []
+nums.append(input("Input a list of numbers"))
+
+
+# In[25]:
+
+
+print (nums)
+
+
+# In[30]:
+
+
+def square(x):
+    
+    y = x*x
+    
+    return y
+
+
+# In[ ]:
+
+
+
+
+
+# In[31]:
+
+
+a = 10
+b = square(a)
+
+
+# In[32]:
+
+
+print (b)
+
+
+# In[34]:
+
+
+def greaterThan (x, y):
+    if x>y:
+        return True
+    else:
+        return False
+
+
+# In[35]:
+
+
+a = 2
+b = 3
+result = greaterThan (a, b)
+print (result)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
